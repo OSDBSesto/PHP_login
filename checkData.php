f07fd4cfd1447bc852c2db09f5c74d2670a8cc3c
Alessandro Bertelli <bertellialessandro02@gmail.com>
2020-11-13 16:33:38 +0100

    fix password read

--- checkData.php
+++ checkData.php
@@ -1,8 +1,38 @@
-<html>
-    <head>
-        <title>Verifica delle credenziali</title>
-    </head>
-    <body>
+<?php
+$path = "credenziali.txt";
+$credenziali;
+if ($fileCredenziali = fopen($path, "r")) {
+    while (!feof($fileCredenziali)) {
         
-    </body>
+        $linea = fgets($fileCredenziali);
+        $nomeUtente = substr($linea, 0, strpos($linea, ";"));
+
+        // sono obbligato a fare questo pasticcio per togliere l'ultimo carattere finale della
+        // password poichè si buggava con il <br />
+        // sono arrivato a questa soluzione anche se moolto brutta dopo 3 ore di troubleshooting e test
+        $password = substr($linea, strpos($linea, ";") + 1, strpos($linea, "|") - 1);
+        $password = $password . " .";
+        $password = substr($password,0, strpos($password, "|"));
+
+    }
+    fclose($fileCredenziali);
+} else {
+    ("Impossibile aprire il file!");
+}
+
+?>
+
+<!-- VIEW -->
+<html>
+
+<head>
+    <title>Verifica delle credenziali</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="stileLogin.css" rel="stylesheet" type="text/css"> <!-- Connessione con file CSS -->
+</head>
+
+<body>
+
+</body>
+
 </html>
