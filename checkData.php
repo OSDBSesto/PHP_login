0f414f3ef83332e2f29a663a23be82c4645edfc7
Alessandro Bertelli <bertellialessandro02@gmail.com>
2020-11-14 14:03:18 +0100

    refactor script php in a file

--- checkData.php
+++ checkData.php
@@ -1,34 +1,7 @@
 <!-- POPOLAZIONE ARRAY -->
 <?php
-// dichiaro nome file
-$path = "credenziali.txt";
-//istanzio fuori dal contesto del while var generica credenziali che diventerà array associativo
-$_CREDENZIALI;
-//creo  hendle del file e verifico che esista
-if ($fileCredenziali = fopen($path, "r")) {
-    while (!feof($fileCredenziali)) {
-        // lettura della stringa
-        $linea = fgets($fileCredenziali);
-        // estraggo dalla linea il nome utente e di seguito la password
-        $nomeUtente = substr($linea, 0, strpos($linea, ";"));
-        /* sono obbligato a fare questo pasticcio per togliere l'ultimo carattere finale della
-           password poichè si buggava con il <br />
-           sono arrivato a questa soluzione anche se moolto brutta dopo 3 ore di troubleshooting e test */
-        $password = substr($linea, strpos($linea, ";") + 1, strpos($linea, "|") - 1);
-        $password = $password . " .";
-        $password = substr($password, 0, strpos($password, "|"));
-
-        /*indicizzo al nome utente la rispettiva password, questo tipo di indicizzazione anche se potrebbe
-        sembrare inizialmente confusionaria e non efficace penso sia utile nell'eventualità di dover salvare 
-        molte informazioni relative ad utente in seguito es. Pw Email Sesso Tel Indirizzo etc...*/
-        $_CREDENZIALI[$nomeUtente] = $password;
-    }
-    // chiudo stream al file
-    fclose($fileCredenziali);
-} else {
-    //comunico l'impossibilità di apertura del file
-    echo ("Impossibile aprire il file!");
-}
+$path = "scriptCredenziali.php";
+include($path);
 ?>
 
 <html>
@@ -47,7 +20,7 @@ if ($fileCredenziali = fopen($path, "r")) {
             if (isset($_CREDENZIALI[$_POST["username"]])) {
                 if ($_POST["psw"] == $_CREDENZIALI[$_POST["username"]]) {
                     echo "<div class=\"container\"><h2>Benvenuto nella tua area dedicata, </h2>";
-                    echo "<h1 style=\"color:green\">".$_POST["username"]."</h1>";
+                    echo "<h1 style=\"color:green\">" . $_POST["username"] . "</h1>";
                     echo "</div>";
                 } else {
                     echo "<img src=\"https://i0.wp.com/vincenttechblog.com/wp-content/uploads/2018/02/lock-pc-wrong-password.jpg?ssl=1\" class =\"avatar\">";
--- forgotPassword.php
+++ forgotPassword.php
@@ -1,9 +1,34 @@
+<?php
+
+//popolo file e array solamente se l'utente mi ha già passato il parametro username
+if (isset($_POST["username"])) {
+    $path = "scriptCredenziali.php";
+    include($path);
+}
+?>
+
 <html>
+
 <head>
     <title>Recupera Password</title>
     <link href="stileLogin.css" rel="stylesheet" type="text/css"> <!-- Connessione con file CSS -->
 </head>
+
 <body>
-    <h1>Pagina sotto costruzione</h1>
+    <h1>Pagina recupero credenziali</h1>
+    <form action="/esercizi/forgotPassword.php" method="post">
+        <div class="imgcontainer">
+            <img src="https://www.piemontetopnews.it/wp-content/uploads/2018/08/Giovanni-Bosco-1-1024x547.jpg" class="avatar">
+        </div>
+        <div class="container">
+            <label for="username"><b>Username</b></label>
+            <input type="text" placeholder="Inserisci Username" name="username" required>
+            <button type="submit">Login</button>
+        </div>
+        <div class="container" style="background-color:#f1f1f1">
+            <span class="psw">Torna a <a href="/esercizi/loginForm.php">login</a></span>
+        </div>
+    </form>
 </body>
+
 </html>
